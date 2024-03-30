31fa410edbfe481bff8db9b4e9d7c04f851f9388
desainanm <desaianm@sheridancollege.ca>
2024-03-30 16:41:31 -0400

    updated tools.py

--- tools.py
+++ tools.py
@@ -6,26 +6,29 @@ import os
 
 
 def resume_into_json(resume):
+    api_key = os.getenv("CLAUDE_API_KEY")
+    client = anthropic.Anthropic(
+        api_key=api_key,
+    )
+
+
     pdf_reader = PdfReader(resume)
     text=""
     for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_num]
-        text += page.extract_text()
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
 
-    client = OpenAI()
+    prompt = f" Act as Master in extracting data from resume.don't give any explantion. please analyze and convert resume data from this {text} into  json, remove data like name, email or personal information and  please return only json file  "
 
-    prompt = f" don't give any explantion. please analyze and convert file data from this {text} into  json and in response please return only json file please don't enter data in fields if irrelevant to template"
+    response = client.messages.create(
+                model="claude-3-haiku-20240307",
+                max_tokens=4000,
+                messages=[
+                    {"role": "user", "content": prompt}
+            ]
+        ).content[0].text
 
-    response = client.chat.completions.create(
-            model="gpt-4-0125-preview",
-              response_format={ "type": "json_object" },  # Adjust the model identifier as needed
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-            {"role": "user", "content": prompt}
-        ],temperature=0.4
-    )
-        
-    return json.loads(response.choices[0].message.content)
+    return json.loads(response)
 
 def company_url(company):
 
